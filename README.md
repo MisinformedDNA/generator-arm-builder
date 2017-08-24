2ba27e44969977d15b9476c950c3f89386703be8
MisinformedDNA <MisinformedDNA@users.noreply.github.com>
2017-08-23 23:16:40 -0500

    Initial checkin

---
+++ README.md
@@ -0,0 +1,36 @@
+# generator-arm-builder [![NPM version][npm-image]][npm-url] [![Build Status][travis-image]][travis-url] [![Dependency Status][daviddm-image]][daviddm-url] [![Coverage percentage][coveralls-image]][coveralls-url]
+> Azure Resource Manager template builder
+
+## Installation
+
+First, install [Yeoman](http://yeoman.io) and generator-arm-builder using [npm](https://www.npmjs.com/) (we assume you have pre-installed [node.js](https://nodejs.org/)).
+
+```bash
+npm install -g yo
+npm install -g generator-arm-builder
+```
+
+Then generate your new project:
+
+```bash
+yo arm-builder
+```
+
+## Getting To Know Yeoman
+
+ * Yeoman has a heart of gold.
+ * Yeoman is a person with feelings and opinions, but is very easy to work with.
+ * Yeoman can be too opinionated at times but is easily convinced not to be.
+ * Feel free to [learn more about Yeoman](http://yeoman.io/).
+
+## License
+
+Apache-2.0 © [Dan Friedman]()
+
+
+[npm-image]: https://badge.fury.io/js/generator-arm-builder.svg
+[npm-url]: https://npmjs.org/package/generator-arm-builder
+[travis-image]: https://travis-ci.org/MisinformedDNA/generator-arm-builder.svg?branch=master
+[travis-url]: https://travis-ci.org/MisinformedDNA/generator-arm-builder
+[daviddm-image]: https://david-dm.org/MisinformedDNA/generator-arm-builder.svg?theme=shields.io
+[daviddm-url]: https://david-dm.org/MisinformedDNA/generator-arm-builder
---
+++ generators/app/index.js
@@ -0,0 +1,92 @@
+'use strict';
+const Generator = require('yeoman-generator');
+const chalk = require('chalk');
+const yosay = require('yosay');
+
+module.exports = class extends Generator {
+  prompting() {
+    // Have Yeoman greet the user.
+    this.log(yosay(
+      'Welcome to the unreal ' + chalk.red('Azure ARM template') + ' generator!'
+    ));
+
+    const prompts = [{
+      type: 'checkbox',
+      name: 'resources',
+      message: 'What components should exist in the template?',
+      choices: [
+        { name: 'Web App', value: 'webapp', checked: true },
+        { name: "-- Application Insights", value: 'webapp_applicationinsights', checked: true },
+        { name: "-- Auto Scale", value: 'webapp_autoscale' },
+        { name: 'SQL Database', value: 'sqldb', checked: true }
+      ],
+      validate: function (answers) {
+        if (answers.length < 1) {
+          return "You must select at least one resource";
+        }
+
+        for (var i = 0; i < answers.length; i++) {
+          var answer = String(answers[i]);
+
+          var split = answer.split("_");
+          if (split.length == 1) { continue; }
+
+          var dependency = split[0];
+          if (answers.findIndex(a => a == dependency) == -1) {
+            return "If you select a child option, you must also select its parent";
+          }
+        }
+
+        return true;
+      }
+    }];
+
+    return this.prompt(prompts).then(props => {
+      // To access props later use this.props.someAnswer;
+      this.props = props;
+    });
+  }
+
+  writing() {
+    // this.fs.copy(
+    //   this.templatePath('webapp.json'),
+    //   this.destinationPath('azuredeploy.json')
+    // );
+
+    var destinationPath = this.destinationPath('azuredeploy.json');
+    var destinationParametersPath = this.destinationPath('azuredeploy.parameters.json');
+    var resources = [];
+
+    this.fs.copy(
+      this.templatePath('empty.json'),
+      destinationPath
+    );
+    this.fs.copy(
+      this.templatePath('empty.parameters.json'),
+      destinationParametersPath
+    );
+
+    this.props.resources.forEach(function (element) {
+      //      var containsDependency = String(element).split("_")
+      var templatePath = this.templatePath(element + ".json");
+
+      // Merge document objects
+      var contents = this.fs.readJSON(this.templatePath(element + ".json"));
+      this.fs.extendJSON(destinationPath, contents);
+
+      resources.push(contents["resources"]);
+
+      contents = this.fs.readJSON(this.templatePath(element + ".parameters.json"));
+      this.fs.extendJSON(destinationParametersPath, contents);
+    }, this);
+
+    // Set merged resources array
+    var all = this.fs.readJSON(destinationPath);
+    all.resources = resources;
+    this.fs.writeJSON(destinationPath, all);
+  }
+
+  install() {
+    //this.installDependencies();
+  }
+};
